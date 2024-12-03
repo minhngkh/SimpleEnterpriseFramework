3aba5c070e2ffcdb88be6093674f6a03a7eb557b
pross1312 <dangvinhtuong12@gmail.com>
2024-12-03 20:27:54 +0700

    Display all tables of a database

--- SimpleEnterpriseFramework.WebApp/Program.cs
+++ SimpleEnterpriseFramework.WebApp/Program.cs
@@ -1,5 +1,19 @@
 using HandlebarsDotNet;
 using System.IO;
+using System.Web;
+using System.Diagnostics;
+using Microsoft.Extensions.Primitives;
+
+struct Product {
+    [DbField("INTEGER", Unique = true, IsKey = true)]
+    public int Id;
+
+    [DbField("TEXT", Nullable = false)]
+    public string Name;
+
+    [DbField("REAL", Nullable = false)]
+    public float Price;
+}
 
 struct User {
     [DbField("INTEGER", Unique = true, IsKey = true)]
@@ -24,7 +38,14 @@ public class Program {
         List<String> tableNames = repo.ListTables();
 
         repo.CreateTable<User>(true);
+        repo.CreateTable<Product>(true);
+
         for (int i = 0; i < 15; i++) {
+            repo.Add<Product>(new Product() {
+                Id = i,
+                Name = $"product{i}",
+                Price = i*1000.0f,
+            });
             repo.Add<User>(new User() {
                 Id = i,
                 Username = $"user{i}",
@@ -47,17 +68,31 @@ public class Program {
 
         var app = builder.Build();
 
-        string tableTemplatePart = Path.Combine(Directory.GetCurrentDirectory(), "templates", "table.hbs");
-        string tableTemplate = File.ReadAllText(tableTemplatePart);
+        string tableTemplatePath = Path.Combine(Directory.GetCurrentDirectory(), "templates", "table.hbs");
+        string tableTemplate = File.ReadAllText(tableTemplatePath);
+        var tablePart = Handlebars.Compile(tableTemplate);
         Handlebars.RegisterTemplate("table", tableTemplate);
 
-        string indexTemplatePart = Path.Combine(Directory.GetCurrentDirectory(), "templates", "index.hbs");;
-        string indexTemplate = File.ReadAllText(indexTemplatePart);
+        string indexTemplatePath = Path.Combine(Directory.GetCurrentDirectory(), "templates", "index.hbs");;
+        string indexTemplate = File.ReadAllText(indexTemplatePath);
         var indexPage = Handlebars.Compile(indexTemplate);
         var tableData = getTableParameters("User");
+
         app.MapGet("/", (HttpContext context) => {
             context.Response.ContentType = "text/html";
-            return indexPage(tableData);
+            return indexPage(new {
+                tableNames = repo.ListTables(),
+                // data = tableData,
+            });
+        });
+        app.MapGet("/table", (HttpContext context) => {
+            StringValues strings;
+            context.Request.Query.TryGetValue("tableName", out strings);
+            Debug.WriteLine(strings.ToString());
+            if (strings.Count > 0) {
+                return tablePart(getTableParameters(strings[0]!));
+            }
+            throw new Exception("Invalid usage of route /table, require query parameter 'tableName'");
         });
         app.Run();
     }
