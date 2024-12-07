c6e27f50207ab4cf38c2adddb9676b8126018532
HoangNA1911 <anhhoangdl12@gmail.com>
2024-12-07 23:43:27 +0700

    Implement MembershipRepository

---
+++ SimpleEnterpriseFramework.Membership/MembershipRepository.cs
@@ -0,0 +1,177 @@
+using System.Security.Cryptography;
+
+namespace SimpleEnterpriseFramework.Membership;
+
+
+struct Role {
+    [DbField("INTEGER", Unique = true, IsKey = true)]
+    public int Id;
+
+    [DbField("TEXT", Unique = true, Nullable = false)]
+    public string Name;
+}
+
+struct User {
+    [DbField("INTEGER", Unique = true, IsKey = true)]
+    public int Id;
+
+    [DbField("TEXT", Unique = true, Nullable = false)]
+    public string Username;
+
+    [DbField("TEXT", Nullable = false)]
+    public string Password;
+
+    [DbField("INTEGER", "Role", "Id", Nullable = false)]
+    public int RoleId;
+}
+public class MembershipRepository
+{
+    
+    IRepository _repository = new SqliteRepository("Data Source=new.db");
+    public void CreateUserTable()
+    {
+        // Create user table by calling repository
+        try
+        {
+            _repository.CreateTable<User>();
+            Console.WriteLine("User table created successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while creating the user table: {ex.Message}");
+        }
+    }
+
+    public void CreateRoleTable()
+    {
+        try
+        {
+            _repository.CreateTable<Role>();
+            Console.WriteLine("Role table created successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while creating the role table: {ex.Message}");
+        }
+    }
+
+    public void CreateKeyTable()
+    {
+
+    }
+
+    public void AddUser(String userName, String password, String roleName)
+    {
+        List<Role> roles =  _repository.Find<Role>(new {Name = roleName});
+        if (roles.Count == 0)
+        {
+            Console.WriteLine("Role name does not exist.");
+            return;
+        }
+        try
+        {
+            _repository.Add(new User()
+            {
+                Id = RandomNumberGenerator.GetInt32(0, int.MaxValue),
+                Username = userName,
+                Password = password,
+                RoleId = roles[0].Id,
+            });
+            Console.WriteLine("User added successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while creating the user table: {ex.Message}");
+        }
+    }
+
+    public void AddRole(String roleName)
+    {
+        List<Role> roles =  _repository.Find<Role>(new {Name = roleName});
+        if (roles.Count != 0)
+        {
+            Console.WriteLine("Role name exists.");
+        }
+        try
+        {
+            _repository.Add(new Role()
+            {
+                Id = RandomNumberGenerator.GetInt32(0, int.MaxValue),
+                Name = roleName,
+            });
+            Console.WriteLine("Role added successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while creating the role table: {ex.Message}");
+        }
+    }
+
+    public void DeleteUser(String userName)
+    {
+        List<User> users = _repository.Find<User>(new { Username = userName });
+        if (users.Count == 0)
+        {
+            Console.WriteLine("User not found.");
+            return;
+        }
+        try
+        {
+            _repository.DeleteRow("User", new { Username = userName });
+            Console.WriteLine(users[0].Username + " deleted successfully.");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Could not delete user.: {ex.Message}");
+        }
+    }
+
+    public void DeleteRole(String roleName)
+    {
+        List<Role> roles = _repository.Find<Role>(new { Name = roleName });
+        if (roles.Count == 0)
+        {
+            Console.WriteLine("Role not found.");
+            return;
+        }
+        try
+        {
+            _repository.DeleteRow("Role", new { Name = roleName });
+            Console.WriteLine(roles[0].Name + " deleted successfully.");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Could not delete role.: {ex.Message}");
+        }
+    }
+
+    public void modifyUser(String userNameIdentify, String userName, String password, String roleName)
+    {
+        List<Role> roles = _repository.Find<Role>(new { Name = roleName });
+        if (roles.Count == 0)
+        {
+            Console.WriteLine("Role not found.");
+            return;
+        }
+
+        try
+        {
+            _repository.UpdateRowBuilder()
+                .SetTable("User")
+                .SetCondition(new { Username = userNameIdentify })
+                .SetUpdateStatement(new
+                {
+                    Username = userName, 
+                    Password = password,
+                    RoleId = roles[0].Id,
+                })
+                .Update();
+            Console.WriteLine("User updated successfully.");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Could not modify user.: {ex.Message}");
+        }
+    }
+    
+}
--- SimpleEnterpriseFramework.WebApp/Program.cs
+++ SimpleEnterpriseFramework.WebApp/Program.cs
@@ -1,46 +1,38 @@
 using HandlebarsDotNet;
 using System.IO;
+using SimpleEnterpriseFramework.Membership;
 
-struct User {
-    [DbField("INTEGER", Unique = true, IsKey = true)]
-    public int Id;
-
-    [DbField("TEXT", Unique = true, Nullable = false)]
-    public string Username;
-
-    [DbField("TEXT", Unique = true, Nullable = false)]
-    public string Email;
-
-    [DbField("TEXT")]
-    public string? Phone;
-
-    [DbField("TEXT", Nullable = false)]
-    public string Password;
-}
 
 public class Program {
     public static void Main(string[] args) {
-        IRepository repo = new SqliteRepository("Data Source=test.db");
-        List<String> tableNames = repo.ListTables();
+        MembershipRepository repo = new MembershipRepository();
+        // List<String> tableNames = repo.ListTables();
 
-        repo.CreateTable<User>(true);
-        for (int i = 0; i < 15; i++) {
-            repo.Add<User>(new User() {
-                Id = i,
-                Username = $"user{i}",
-                Email = $"example{i}@gmail.com",
-                Phone = i%2 == 0 ? null : String.Concat(Enumerable.Repeat($"{i}", 10)),
-                Password = $"password{i}"
-            });
-        }
+        // repo.CreateUserTable();
+        // repo.CreateRoleTable();
+        // repo.AddRole("admin");
+        // repo.AddRole("user");
+        // repo.AddUser("anhhoang123", "123", "admin");
+        // repo.DeleteUser("anhhoang");
+           repo.modifyUser("anhhoang123", "anh", "456", "user");
+        
+        // for (int i = 0; i < 15; i++) {
+        //     repo.Add<User>(new User() {
+        //         Id = i,
+        //         Username = $"user{i}",
+        //         Email = $"example{i}@gmail.com",
+        //         Phone = i%2 == 0 ? null : String.Concat(Enumerable.Repeat($"{i}", 10)),
+        //         Password = $"password{i}"
+        //     });
+        // }
 
-        object getTableParameters(string tableName) {
-            return new {
-                tableName = tableName,
-                columns = repo.ListColumns(tableName).Select(colInfo => colInfo.name).ToArray(),
-                data = repo.Find(tableName),
-            };
-        }
+        // object getTableParameters(string tableName) {
+        //     return new {
+        //         tableName = tableName,
+        //         columns = repo.ListColumns(tableName).Select(colInfo => colInfo.name).ToArray(),
+        //         data = repo.Find(tableName),
+        //     };
+        // }
 
 
         var builder = WebApplication.CreateSlimBuilder(args);
@@ -54,11 +46,11 @@ public class Program {
         string indexTemplatePart = Path.Combine(Directory.GetCurrentDirectory(), "templates", "index.hbs");;
         string indexTemplate = File.ReadAllText(indexTemplatePart);
         var indexPage = Handlebars.Compile(indexTemplate);
-        var tableData = getTableParameters("User");
-        app.MapGet("/", (HttpContext context) => {
-            context.Response.ContentType = "text/html";
-            return indexPage(tableData);
-        });
+        // var tableData = getTableParameters("User");
+        // app.MapGet("/", (HttpContext context) => {
+        //     context.Response.ContentType = "text/html";
+        //     return indexPage(tableData);
+        // });
         app.Run();
     }
 }
